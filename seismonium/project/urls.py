d3237f530499709e5712f8f6f6b6e121b12e7bd1
Sean O'Donnell <sean.reads@gmail.com>
2013-10-28 16:32:49 -0400

    wired up routes to root url

--- seismonium/project/urls.py
+++ seismonium/project/urls.py
@@ -14,6 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^maps/', include('maps.urls')),
-    url(r'^earthquakes/', include('maps.urls'))
+    url(r'^', include('maps.urls'))
 )
